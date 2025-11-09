0fce249c545bf1b7dc7fb3c394ad8cb5ee0e7926
ifeomasylviadike <ifeomasylvia.dike@gmail.com>
2025-11-09 11:31:35 +0100

    removed export function

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -1,33 +1,59 @@
+/// <reference path="./crud.d.ts" />
+
+import { RowID, RowElement } from './interface';
+import * as CRUD from './crud.js';
+
+const row: RowElement = {
+  firstName: 'Guillaume',
+  lastName: 'Salva',
+};
+
+const newRowID: RowID = CRUD.insertRow(row);
+// Insert row {firstName: "Guillaume", lastName: "Salva"}
+
+const updatedRow: RowElement = { ...row, age: 23 };
+
+CRUD.updateRow(newRowID, updatedRow);
+// Update row 125 {firstName: "Guillaume", lastName: "Salva", age: 23}
+
+CRUD.deleteRow(newRowID);
+// Delete row id 125
+
+
+
 /// <reference path="subjects/Teacher.ts" />
 /// <reference path="subjects/Subject.ts" />
 /// <reference path="subjects/Cpp.ts" />
 /// <reference path="subjects/Java.ts" />
 /// <reference path="subjects/React.ts" />
 
-export const cpp = new Subjects.Cpp();
-export const java = new Subjects.Java();
-export const react = new Subjects.React();
+namespace Subjects {
+  const cpp = new Cpp();
+  const java = new Java();
+  const react = new React();
 
-export const cTeacher: Subjects.Teacher = {
-  firstName: 'John',
-  lastName: 'Doe',
-  experienceTeachingC: 10,
-};
+  const cTeacher: Teacher = {
+    firstName: 'John',
+    lastName: 'Doe',
+    experienceTeachingC: 10,
+  };
 
-// C++
-console.log('C++');
-cpp.setTeacher(cTeacher);
-console.log(cpp.getRequirements());
-console.log(cpp.getAvailableTeacher());
+  // C++
+  console.log('C++');
+  cpp.setTeacher(cTeacher);
+  console.log(cpp.getRequirements());
+  console.log(cpp.getAvailableTeacher());
 
-// Java
-console.log('Java');
-java.setTeacher(cTeacher);
-console.log(java.getRequirements());
-console.log(java.getAvailableTeacher());
+  // Java
+  console.log('Java');
+  java.setTeacher(cTeacher);
+  console.log(java.getRequirements());
+  console.log(java.getAvailableTeacher());
 
-// React
-console.log('React');
-react.setTeacher(cTeacher);
-console.log(react.getRequirements());
-console.log(react.getAvailableTeacher());
+  // React
+  console.log('React');
+  react.setTeacher(cTeacher);
+  console.log(react.getRequirements());
+  console.log(react.getAvailableTeacher());
+
+  export { cpp, java, react, cTeacher };
