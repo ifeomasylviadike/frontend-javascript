851dc59551dce752fcfcd9771fe687c2d2d35653
ifeomasylviadike <ifeomasylvia.dike@gmail.com>
2025-11-09 03:25:39 +0100

    TypeScript Class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -20,5 +20,36 @@ function printTeacher( { firstName, lastName }: { firstName: string, lastName: s
   return `${firstName}. ${lastName}`; }
 
 
+  // 1️⃣ Interface for the constructor
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+// 2️⃣ Interface describing the class
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+// 3️⃣ Class implementing the interface
+class StudentClass implements StudentClassInterface {
+  private firstName: string;
+  private lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+
 
 
